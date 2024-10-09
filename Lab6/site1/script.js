a4a243b745be904b67d72cebf02bea24832fe69b
mluud <maretluud@gmail.com>
2024-10-09 08:40:24 +0300

    Lab5 site 2, Lab6 site 1
    Working on the Lab5 layout exercise and Lab6 classwork.

---
+++ Lab6/site1/script.js
@@ -0,0 +1,25 @@
+
+//let answer=prompt("question?")
+//console.log(answer)
+//document.write("My name is Maret")
+
+//let def = new Date();
+//let abc = def.toString();
+//console.log(abc)
+
+//let date= new Date ();
+//console.log(date.toString());
+
+//let date= new Date ();
+//console.log(date);
+
+//let date= new Date ();
+//console.log(date.getDay());
+
+//let x=5, y=12;
+//let result = x - y;
+//console.log(result);
+
+let x=12, y=5;
+let result = x % y;
+console.log(result);
